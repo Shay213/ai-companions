6cf4e3d265b2d3228aecb1c14f3a62b9ec4b4b1f
shay213 <dawidpawelec@o2.pl>
2023-08-10 19:07:38 +0200

    Add clerk authenticaiton

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,14 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 type Props = {};
 
 const Home = (props: Props) => {
-  return <div className="text-red-600">Home</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default Home;
