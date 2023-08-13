6b2de508cba356a92963609c98618480e9fdaa01
shay213 <dawidpawelec@o2.pl>
2023-08-13 17:31:48 +0200

    Add companions grid to home page

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,16 +1,45 @@
 import Categories from "@/components/Categories";
+import Companions from "@/components/Companions";
 import SearchInput from "@/components/SearchInput";
 import { prisma } from "@/lib/prismadb";
 import React from "react";
 
-type Props = {};
+type Props = {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  };
+};
 
-const Home = async (props: Props) => {
-  const categories = await prisma.category.findMany();
+const Home = async ({ searchParams: { categoryId, name } }: Props) => {
+  const categoriesPromise = prisma.category.findMany();
+  const companionsPromise = prisma.companion.findMany({
+    where: {
+      categoryId,
+      name: { contains: name },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  });
+
+  const [categories, companions] = await Promise.all([
+    categoriesPromise,
+    companionsPromise,
+  ]);
+
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
       <Categories categories={categories} />
+      <Companions companions={companions} />
     </div>
   );
 };
